f7c4cb718c39bc2c808ce243ca4346e2fcf2557a
Emir Burgos Paz <47346304+EmirBurgosPaz@users.noreply.github.com>
2022-11-29 00:25:07 -0600

    test mobile 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -119,9 +119,6 @@
 })(jQuery);
 
 
-var scale = screen.width /siteWidth;
-
-document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'')
 
 function openPage(pageName, elmnt, color) {
 	// Hide all elements with class="tabcontent" by default */
