4f5161368f81dc1de65f7d46e2cbfd42413cd4b9
Emir Burgos Paz <47346304+EmirBurgosPaz@users.noreply.github.com>
2022-12-04 19:43:15 -0600

    3/4 pesañas completas

--- assets/js/main.js
+++ assets/js/main.js
@@ -145,3 +145,7 @@ function openPage(pageName, elmnt, color) {
 	document.getElementById("defaultOpen").click(); x``
 
 
+	function topFunction() {
+		document.body.scrollTop = 0; // For Safari
+	    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+	  } 
